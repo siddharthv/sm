6890f2a9539386c1e90a268e5e6c6b64c14b51a7
Robert Breker <robert.breker@citrix.com>
2014-06-22 10:34:07 +0100

    CA-141409: [UnitTest] Updated unittests to default to using NFS version 3 again
    Signed-off-by: Robert Breker <robert.breker@citrix.com>

--- tests/test_nfs.py
+++ tests/test_nfs.py
@@ -20,9 +20,10 @@ class Test_nfs(unittest.TestCase):
         pread.assert_called_once_with(['/usr/sbin/rpcinfo', '-t', 'aServer',
                                       'nfs', 'aNfsversion'])
 
-    def get_soft_mount_pread(self, binary):
+    def get_soft_mount_pread(self, binary, vers):
         return ([binary, 'remoteserver:remotepath', 'mountpoint', '-o',
-                 'soft,timeo=600,retrans=2147483647,proto=transport,actimeo=0']
+                 'soft,timeo=600,retrans=2147483647,proto=transport,'
+                 'vers=%s,actimeo=0' % (vers)]
                 )
 
     @mock.patch('util.makedirs')
@@ -31,7 +32,8 @@ class Test_nfs(unittest.TestCase):
         nfs.soft_mount('mountpoint', 'remoteserver', 'remotepath', 'transport',
                        timeout=0)
 
-        pread.assert_called_once_with(self.get_soft_mount_pread('mount.nfs'))
+        pread.assert_called_once_with(self.get_soft_mount_pread('mount.nfs',
+                                                                '3'))
 
     @mock.patch('util.makedirs')
     @mock.patch('util.pread')
@@ -39,7 +41,8 @@ class Test_nfs(unittest.TestCase):
         nfs.soft_mount('mountpoint', 'remoteserver', 'remotepath', 'transport',
                        timeout=0, nfsversion='3')
 
-        pread.assert_called_once_with(self.get_soft_mount_pread('mount.nfs'))
+        pread.assert_called_once_with(self.get_soft_mount_pread('mount.nfs',
+                                                                '3'))
 
     @mock.patch('util.makedirs')
     @mock.patch('util.pread')
@@ -47,7 +50,8 @@ class Test_nfs(unittest.TestCase):
         nfs.soft_mount('mountpoint', 'remoteserver', 'remotepath', 'transport',
                        timeout=0, nfsversion='4')
 
-        pread.assert_called_once_with(self.get_soft_mount_pread('mount.nfs4'))
+        pread.assert_called_once_with(self.get_soft_mount_pread('mount.nfs4',
+                                                                '4'))
 
     def test_validate_nfsversion_invalid(self):
         for thenfsversion in ['2', '4.1']:
